e1add6f7bcb8b797c26434648c134ee4734129b6
Dev-KhalidAi <77898059+Dev-KhalidAi@users.noreply.github.com>
2021-07-14 13:32:11 +0300

    First Touches
    Create the first part of the landing page

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vehicle Rent Management System</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://storage.googleapis.com/graph-fonts/EuclidCircular/fonts.css">
+
+</head>
+
+<body>
+    <div class="main-container">
+        <div class="main-section">
+            <div class="header">
+                <img class= "logo" src="./images/logo-white.png" alt="">
+                <ul>
+                    <li>Home</li>
+                    <li>Vehicles</li>
+                    <li>How To Use</li>
+                    <li>About Us</li>
+                   
+                </ul>
+                <ul class="sign-in-up">
+                    <li id="signs">Sign In</li>
+                    <li id="signs">Sign Up</li>
+                </ul>
+            </div>
+            <section>
+                <div class="intro">
+                    Welcome To Khalid's Vehicle <br> Rent Management System
+                </div><br>
+                <div class="describtion">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ab cum officia esse ipsam laboriosam optio. Quaerat, doloribus unde? Accusantium, laudantium. Labore dignissimos sequi repellat in, labori Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam incidunt magni earum nostrum exercitationem qui tenetur autem, quidem a sit tempore illum ex delectus corporis necessitatibus fuga, voluptas sapiente enim!
+                </div>
+
+                <button class="book-button"> Book Now</button>
+            </section>
+            <br><br><br><br><br><br>
+              <div class="seprator">
+                <div class="square1"></div>
+                <div class="line"></div>
+                <div class="square1"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
