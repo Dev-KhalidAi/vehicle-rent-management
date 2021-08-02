450ee33ebc7e5a438be1d7136d3ab75256f160dc
Khalid Awlaqi <77898059+Dev-KhalidAi@users.noreply.github.com>
2021-08-02 15:27:36 +0300

    adding stuff

---
+++ scripts/header-script.js
@@ -0,0 +1,15 @@
+const header = document.querySelector(".header");
+
+window.onscroll = () => {
+  if (this.scrollY >= 200) {
+    header.style.background = "black";
+    header.style.zIndex = "2";
+    header.style.padding = "0px 50px 0px 50px";
+    header.style.borderRadius = " 0px 0px 20px 20px";
+    header.style.top = "0";
+  } else {
+    header.style.top = "-100px";
+    header.style.background = "";
+    header.style.width = "";
+  }
+};
